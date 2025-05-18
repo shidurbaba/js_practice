f0c5b70c7c2bf6157142f5ffb44e7743edb94030
shidurbaba <30811546+shidurbaba@users.noreply.github.com>
2025-05-18 13:30:59 -0400

    js_practice

---
+++ index.js
@@ -0,0 +1,53 @@
+// console.log(`Hello, world!`);
+// console.log(`I like to code!`)
+
+// window.alert(`This is an alert!`);
+// window.alert(`I like to eat pizza!`);
+
+//This is a comment
+
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myP").textContent = `I like to code`
+
+//variable - A container that stores a value
+//Behaves as if it were the value it contains
+
+// 1. declaration - let x;
+// 2. assignment x = 100;
+
+// let fullName = "Moses Peris"
+// let age = 35
+// let student = false
+
+// document.getElementById("p1").textContent = fullName;
+// document.getElementById("p2").textContent = age;
+// document.getElementById("p3").textContent = student;
+
+//arithmetic operators = operands (values, variables, etc.)
+//operators (+ - * **)
+// ex. 11 = x + 5
+
+//let students = 30
+
+//Expotent example:
+// students = students ** 3;
+// console.log(expostudents);
+
+//Modules operator
+//students = students % 2;
+
+//Augmented operators
+//students += 1;
+//students -= 1;
+//students *=2
+//students **=2
+//students %= 2
+//students++
+//console.log(students);
+
+//let result = 1 + 2 * 3 + 4 ** 2;
+let result = 12 % 5 + 8 / 2;
+
+console.log(`The result is ${result} `);
+
+
