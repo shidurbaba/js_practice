8c4923ad6b4cf6d7d90fb919adbb349b36378450
shidurbaba <30811546+shidurbaba@users.noreply.github.com>
2025-05-18 13:54:03 -0400

    commit to branch

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // 1. EASY WAY = window prompt
 // 2. PROFESSIONAL WAY = HTML textbox
 
+//1. EASY WAY
+// let username;
+// username = window.prompt("What's your username?");
+// console.log(username);
+
+//2. PROFESSIONAL WAY
 let username;
 
-username = window.prompt("What's your username?");
-
-console.log(username);
+document.getElementById("mySubmit").onclick = function () {
+    username = document.getElementById("myText").value;
+    document.getElementById("myH1").textContent = `Welcome to ${username}`
+}
