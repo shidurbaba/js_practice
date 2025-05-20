67a322ab2f4e5cd7ddf4b484a5cece3ff29466a7
shidurbaba <30811546+shidurbaba@users.noreply.github.com>
2025-05-19 23:23:09 -0400

    committed new code changes

--- js-constant-tutorial/index.js
+++ js-constant-tutorial/index.js
@@ -1,12 +1,14 @@
 // const = a variable that can't be changed
 
-let pi = 3.14159;
+const PI = 3.14159;
 let radius;
 let circumference;
 
-radius = window.prompt('Enter the radius of a circle');
-radius = Number(radius);
 
-circumference = 2 * pi * radius;
 
-console.log(`The radius of the circumference is ${circumference}`);
+document.getElementById("myButton").onclick = function () {
+    radius = document.getElementById("myText").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = circumference + " cm";
+}
